1ca7010f5a8ceb5a789d5926797c44fcb8d0a5b7
Matias Machado de Faria <matiasmf@est.grad.ufmg.br>
2025-06-27 15:40:51 -0300

    Update filme.h

--- Sistema_de_Avaliacao_de_Filmes/src/filme.h
+++ Sistema_de_Avaliacao_de_Filmes/src/filme.h
@@ -5,20 +5,25 @@
 #include <vector>
 #include "avaliacao.h"
 
-
+// Contém título, gênero, avaliações e comentários.
 class Filme
 {
 public:
+    // Criação do filme com título e categoria (gênero)
     Filme(const std::string &titulo, const std::string &genero);
 
+    // Adiciona uma avaliação de um usuário
     void adicionarAvaliacao(const std::string &usuario, int nota);
+    // Adiciona um comentário sobre o filme
     void adicionarComentario(const std::string &comentario);
+    // Calcula a média das avaliações feitas no filme
     double calcularMedia() const;
+    // Mostra todas as informações sobre o filme
     void exibirInformacoes() const;
-
+    // Retorna o título do filme (evitando duplicadas)
     std::string getTituloNormalizado() const;
+    // Retorna a categoria do filme
     std::string getGenero() const;
-
 
 private:
     std::string titulo;
