6eccd3f99d17aab72328d46c94e571cf73805f06
Benascimento04 <benascimento4002@gmail.com>
2025-06-29 20:43:32 -0300

    Update filme.h

--- Sistema_de_Avaliacao_de_Filmes/src/filme.h
+++ Sistema_de_Avaliacao_de_Filmes/src/filme.h
@@ -6,29 +6,26 @@
 #include "avaliacao.h"
 
 // Contém título, gênero, avaliações e comentários.
-class Filme
-{
+class Filme {
 public:
-    // Criação do filme com título e categoria (gênero)
     Filme(const std::string &titulo, const std::string &genero, const std::string &dataLancamento);
 
-    // Adiciona uma avaliação de um usuário
     void adicionarAvaliacao(const std::string &usuario, int nota);
-    // Adiciona um comentário sobre o filme
     void adicionarComentario(const std::string &comentario);
-    // Calcula a média das avaliações feitas no filme
     double calcularMedia() const;
-    // Mostra todas as informações sobre o filme
-    void exibirInformacoes() const; 
-    // Retorna o título original do filme
+    void exibirInformacoes() const;
+
     std::string getTitulo() const;
-    // Retorna o título do filme (evitando duplicadas)
     std::string getTituloNormalizado() const;
-    // Retorna a categoria do filme
     std::string getGenero() const;
-    // Retorna a data de lançamento do filme
     std::string getDataLancamento() const;
-    // Verifica a similaridade entre dois títulos de filme
+
+    // Retorna todas as avaliações
+    std::vector<Avaliacao> getAvaliacoes() const;
+
+    // Retorna todos os comentários
+    std::vector<std::string> getComentarios() const;
+
     static bool compararTitulosSimilaridade(const std::string &titulo1, const std::string &titulo2);
 
 private:
@@ -37,7 +34,6 @@ private:
     std::string dataLancamento;
     std::vector<Avaliacao> avaliacoes;
     std::vector<std::string> comentarios;
-
 };
 
 #endif
