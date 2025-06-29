03b9b98fa9d3c86ea0636714b1aca86a4a6cbd11
Arthur de Almeida <134896540+salozari@users.noreply.github.com>
2025-06-29 20:47:02 -0300

    Update usuario.cpp

--- Sistema_de_Avaliacao_de_Filmes/src/usuario.cpp
+++ Sistema_de_Avaliacao_de_Filmes/src/usuario.cpp
@@ -6,45 +6,41 @@
 #include <algorithm>
 
 Usuario::Usuario(const std::string &nomeCompleto, const std::string &dataNascimento, const std::string &senha)
-    : nomeCompleto(nomeCompleto), dataNascimento(dataNascimento), senha(senha)
-{
-
+    : nomeCompleto(nomeCompleto), dataNascimento(dataNascimento), senha(senha) {
     nomePublico = gerarNomePublico(obterSobrenome());
 }
 
-std::string Usuario::getNomePublico() const
-{
+std::string Usuario::getNomePublico() const {
     return nomePublico;
 }
 
-bool Usuario::autenticar(const std::string &senhaTentativa) const
-{
+bool Usuario::autenticar(const std::string &senhaTentativa) const {
     return senha == senhaTentativa;
 }
 
-std::string Usuario::getNomeCompleto() const
-{
+std::string Usuario::getNomeCompleto() const {
     return nomeCompleto;
 }
 
-std::string Usuario::getDataNascimento() const
-{
+std::string Usuario::getDataNascimento() const {
     return dataNascimento;
 }
 
-std::string Usuario::obterSobrenome() const
-{
+// NOVO: método para retornar a senha do usuário (para salvar em arquivo)
+std::string Usuario::getSenha() const {
+    return senha;
+}
+
+std::string Usuario::obterSobrenome() const {
     std::istringstream iss(nomeCompleto);
     std::string palavra, sobrenome;
-    while (iss >> palavra)
-    {
+    while (iss >> palavra) {
         sobrenome = palavra;
     }
     return sobrenome;
 }
 
-std::string Usuario::gerarNomePublico(const std::string &sobrenome) const
-{
+std::string Usuario::gerarNomePublico(const std::string &sobrenome) const {
     std::vector<std::string> personagens = {
         "Neo", "Trinity", "Yoda", "Leia", "Frodo", "Gandalf", "Vader", "Hermione", "Batman",
         "IronMan", "Thor", "Loki", "BlackPanther", "Joker", "Katniss", "Buzz", "Woody",
@@ -52,4 +48,16 @@ std::string Usuario::gerarNomePublico(const std::string &sobrenome) const
 
     std::string personagem = personagens[rand() % personagens.size()];
     return personagem + " " + sobrenome;
+}
+
+void Usuario::registrarAvaliacao(const std::string& tituloFilme, int nota) {
+    avaliacoes[tituloFilme] = nota;
+}
+
+const std::map<std::string, int>& Usuario::getAvaliacoes() const {
+    return avaliacoes;
+}
+
+bool Usuario::jaAvaliou(const std::string& tituloFilme) const {
+    return avaliacoes.find(tituloFilme) != avaliacoes.end();
 }
